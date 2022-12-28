9dde2d677d630904c699e13691fbd69e31fa79e5
nayan0584 <118335401+nayan0584@users.noreply.github.com>
2022-12-28 19:25:27 +0530

    Working on Header.tsx file

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -4,19 +4,37 @@ import FastImage from 'react-native-fast-image';
 import {
   heightPercentageToDP as hp,
   widthPercentageToDP as wp,
-} from 'react-native-responsive-screen';
-import {images} from '../Constants';
+} from "react-native-responsive-screen";
+import Icons from "react-native-vector-icons/Ionicons";
+import IconsAnt from "react-native-vector-icons/AntDesign";
+import { COLORS, images } from "../Constants";
 
 const Header = () => {
   return (
     <SafeAreaView style={styles.container}>
       {/* Instagram Logo */}
       <View style={styles.logowithMenuContainer}>
-        <FastImage
-          source={images.logo}
-          resizeMode="cover"
-          style={styles.imageContainer}
-        />
+        {/* <View style={styles.logoContainer}>
+          <FastImage
+            source={images.logo}
+            resizeMode="contain"
+            style={styles.logo}
+          />
+          <TouchableOpacity
+            style={styles.dropDownContainer}
+            activeOpacity={0.5}
+          >
+            <Icons name="ios-chevron-down" color={COLORS.black} size={hp(2)} />
+          </TouchableOpacity>
+        </View>
+        <View style={[styles.logoContainer, { width: hp(16), height: hp(7) }]}>
+          <TouchableOpacity
+            style={styles.dropDownContainer}
+            activeOpacity={0.5}
+          >
+            <IconsAnt name="plussquareo" color={COLORS.black} size={hp(2)} />
+          </TouchableOpacity>
+        </View> */}
       </View>
     </SafeAreaView>
   );
@@ -25,15 +43,25 @@ const Header = () => {
 const styles = StyleSheet.create({
   container: {
     height: hp(7),
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   logowithMenuContainer: {
-    flexDirection: 'row',
+    flexDirection: "row",
+    flex: 1,
+    alignItems: "center",
+  },
+  logoContainer: {
+    flex: 1,
+    flexDirection: "row",
     borderWidth: 1,
   },
-  imageContainer: {
-    width: '50%',
+  logo: {
+    width: hp(16),
+    height: hp(7),
+  },
+  dropDownContainer: {
+    alignSelf: "center",
   },
 });
 
